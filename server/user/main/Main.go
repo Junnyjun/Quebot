ff67351088d76df87c05f1a585fa882dc2ea4c14
junny <chbe5082@naver.com>
2024-04-26 00:33:01 +0900

    config setup

--- server/user/main/Main.go
+++ server/user/main/Main.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	"github.com/spf13/viper"
-	"log"
+	"myproject/global"
 )
 
+const env = "dev"
+
 func main() {
-	var env = "development"
-
-	viper.AddConfigPath("../config")
-	viper.SetConfigName("config-" + env)
-	viper.SetConfigType("yaml")
-
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Fatalf("Error reading config file, %s", err)
-	}
-
+	_ = global.GetInstance(env)
 }
